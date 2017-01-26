6072c92c576ecf7920e019a3d3c66cc08f1c07e1
TheTrishMapow <trishmapow@users.noreply.github.com>
2017-01-26 11:35:43 +1000

    Add files via upload

---
+++ Simpliciti_registers.h
@@ -0,0 +1,37 @@
+/***************************************************************
+ *  SmartRF Studio(tm) Export
+ *
+ *  Radio register settings specifed with C-code
+ *  compatible #define statements.
+ *
+ *  RF device: CC1101
+ *
+ ***************************************************************/
+
+#ifndef SMARTRF_CC1101_H
+#define SMARTRF_CC1101_H
+
+#define SMARTRF_RADIO_CC1101
+#define SMARTRF_SETTING_IOCFG0     0x06
+#define SMARTRF_SETTING_FIFOTHR    0x47
+#define SMARTRF_SETTING_PKTCTRL0   0x05
+#define SMARTRF_SETTING_FSCTRL1    0x06
+#define SMARTRF_SETTING_FREQ2      0x10
+#define SMARTRF_SETTING_FREQ1      0xB0
+#define SMARTRF_SETTING_FREQ0      0x5A
+#define SMARTRF_SETTING_MDMCFG4    0xF9
+#define SMARTRF_SETTING_MDMCFG3    0x68
+#define SMARTRF_SETTING_MDMCFG2    0x03
+#define SMARTRF_SETTING_DEVIATN    0x44
+#define SMARTRF_SETTING_MCSM0      0x18
+#define SMARTRF_SETTING_FOCCFG     0x16
+#define SMARTRF_SETTING_WORCTRL    0xFB
+#define SMARTRF_SETTING_FSCAL3     0xE9
+#define SMARTRF_SETTING_FSCAL2     0x2A
+#define SMARTRF_SETTING_FSCAL1     0x00
+#define SMARTRF_SETTING_FSCAL0     0x1F
+#define SMARTRF_SETTING_TEST2      0x81
+#define SMARTRF_SETTING_TEST1      0x35
+#define SMARTRF_SETTING_TEST0      0x09
+
+#endif
